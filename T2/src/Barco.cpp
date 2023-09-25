c287eecda5e0e6898792994c6a2df9466162f8b4
Jacob Gonzalez <jacob.gonga@gmail.com>
2023-09-24 23:51:42 -0600

    Corrigiendo barco.cpp
    Signed-off-by: Jacob Gonzalez <jacob.gonga@gmail.com>

--- T2/src/Barco.cpp
+++ T2/src/Barco.cpp
@@ -2,6 +2,9 @@
 #include <vector>
 #include "Barco.hpp"
 
+Barco::Barco() : latitud(0, 0.0, 'N'), longitud(0, 0.0, 'E') {
+}
+
 void Barco::ingresarCoordenadas() {
   std::cout << "Ingrese las coordenadas de latitud: " << std::endl;
   latitud.ingresarAngulo();
