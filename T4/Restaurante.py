40814c82e024947d2c70f9a876583ceecf0bfe23
Jacob <jacob.gonga@gmail.com>
2023-10-27 09:59:02 -0600

    Modificaciones del constructor y pep8
    Signed-off-by: Jacob <jacob.gonga@gmail.com>

--- T4/Restaurante.py
+++ T4/Restaurante.py
@@ -4,23 +4,18 @@ class Restaurante:
         self.__nombre = ""
         self.__tipo_cocina = ""
 
-    '''def __init__(self, nombre, tipo_cocina):
-        self.__nombre = nombre
-        self.__tipo_cocina = tipo_cocina'''
-
     def get_nombre(self):
         return self.__nombre
-    
+
     def set_nombre(self, nombre):
         self.__nombre = nombre
-    
+
     def get_tipo_cocina(self):
         return self.__tipo_cocina
-    
+
     def set_tipo_cocina(self, tipo_cocina):
         self.__tipo_cocina = tipo_cocina
-    
+
     def describe_restaurante(self):
         print(f"Nombre: {self.__nombre}")
         print(f"Tipo de cocina: {self.__tipo_cocina}")
-
