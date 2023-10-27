8510043d8da185617c4a4fc0a9dcf8bdfa923069
Jacob <jacob.gonga@gmail.com>
2023-10-27 09:56:24 -0600

    Correciones constructor y pep8
    Signed-off-by: Jacob <jacob.gonga@gmail.com>

--- T4/HeladoCacique.py
+++ T4/HeladoCacique.py
@@ -1,12 +1,27 @@
 class HeladoCacique:
     def __init__(self):
         self.__sabores = []
+        self.__sabor = ""
 
     # sobrecarga de operador de suma
     def __add__(self, sabor):
         self.__sabores.append(sabor)
         return self
-    
+
+    def get_sabor(self):
+        return self.__sabor
+
+    def set_sabor(self, sabor):
+        self.__sabor = sabor
+
+    '''
+    Funcion con la que se muestran los sabores registrados
+    '''
     def mostrar_sabores(self):
-        for sabor in self.__sabores:
-            print(sabor)
+        print("Sabores disponibles: ", end="")
+        if len(self.__sabores) == 1:
+            print(self.__sabores[0], end="")
+        else:
+            print(self.__sabores[0], end="")
+            for sabor in self.__sabores[1:]:
+                print(", " + sabor, end="")
