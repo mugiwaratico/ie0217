ba85170cce9352a5f8f02739cf2e9f7d5e0333ce
Jacob <jacob.gonga@gmail.com>
2023-12-02 20:32:52 -0600

    Ultimo ajustes codigo
    Signed-off-by: Jacob <jacob.gonga@gmail.com>

--- T6/datos.py
+++ T6/datos.py
@@ -6,7 +6,6 @@ from sklearn.cluster import KMeans
 from sklearn.linear_model import LinearRegression
 from sklearn import datasets, linear_model
 
-
 # Importacion del archivo de datos
 data = pd.read_csv('T1.csv',index_col=0)
 
@@ -42,13 +41,12 @@ scaled_data = scaler.fit_transform(data)
 
 
 #Grafica de los datos
-'''
 plt.plot(data['Anho'],data['Cantidad'])
 plt.xlabel('Cantidad')
 plt.ylabel('Anho')
 plt.title('Tasa de muertes infantiles anual por cada 1000 nacimientos en Costa Rica')
 plt.show()
-'''
+
 
 # Seleccion numero de clusters
 
@@ -59,13 +57,12 @@ for k in range(1,11):
     inertia.append(kmeans.inertia_)
 
 # Grafica metodo del codo
-'''
+
 plt.plot(range(1,11), inertia, marker = 'o')
 plt.title("Metodo del codo para seleccion de k")
 plt.xlabel('Numero de Clusters (k)')
 plt.ylabel('Inercia')
 plt.show()
-'''
 
 # Basado en la grafica anterior se asume num de cluster = 3
 kmeans = KMeans(n_clusters=3, random_state = 42)
@@ -77,7 +74,6 @@ data['Cluster'] = cluster_labels
 # Analisis de segmentos
 segment_analysis = data.groupby('Cluster').mean(numeric_only=True)
 
-'''
 # Visualizacion de segmentos
 plt.figure(figsize=(12, 6))
 
@@ -85,24 +81,26 @@ for cluster in range(3):
     plt.scatter(data[data['Cluster'] == cluster]['Anho'],
                 data[data['Cluster'] ==  cluster]['Cantidad'],
                 label=f'Cluster {cluster}')
-'''
-'''    
+    
 plt.title('Segmentacion de muertes por Cantidad y Anho')
 plt.xlabel('Anho')
 plt.ylabel('Cantidad')
 plt.legend()
 plt.xticks(fontsize=4)
 plt.show()
-'''
+
 
 # Regresiones
-'''
+
+X = data['Anho']
+y = data['Cantidad']
+
 # Grafico dispersion
 plt.scatter(X, y)
 plt.xlabel("Variable Independiente (X)")
 plt.ylabel("Variable Dependiente (y)")
 plt.show()
-'''
+
 
 # Modelo de regresion lineal 
 modelo =  LinearRegression()
